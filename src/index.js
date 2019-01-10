64f5940d5c0b432e62b72ab7b863137c0c1b4ee9
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-10 09:51:14 +0100

    minor adjustments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-const { exec } = require('child_process')
 const { determineScript } = require('./helpers/determineScript')
-const chalk = require('chalk')
+const { execute } = require('./helpers/execute')
 
 async function main() {
-
-  // Determine command by launching an interactive wizard
+  // Determine which script to run by launching an interactive wizard
   const script = await determineScript()
-  const command = `yarn ${script}`
-  const file = `${script}.js`
 
-  // Run command
-  console.log(`\nRunning command: ${chalk.bold.blue(`$ ${command}`)} ...\n`)
-  exec(command, (err, stdout) => {
-    console.log(chalk.green(stdout))
-    console.log(`\nCheck out ${chalk.bold.blue(file)} to learn more.\n`)
-  })
-
+  // Execute the script
+  await execute(script)
 }
 
 main()
